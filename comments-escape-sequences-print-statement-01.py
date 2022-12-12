d1ac8418e46c937fd10044c3a2e5a1a2f963a31b
Sanjay <sanjaypn18@gmail.com>
2022-12-12 23:21:04 +0530

    first Basic python code

---
+++ comments-escape-sequences-print-statement-01.py
@@ -0,0 +1,66 @@
+#1.Comment Please dont remove this line
+
+"""
+This is a Multiline Comments
+"""
+
+print("Subscribe CodeWithHarry now")
+print("This is for Scripting")
+
+"""
+#output
+Subscribe CodeWithHarry now
+This is for Scripting
+"""
+
+"""
+#2.If you dont want to create new line like above 
+"""
+
+print("Subscribe CodeWithHarry now", end=" ")
+print("This is for Scripting")
+
+
+"""
+#output
+Subscribe CodeWithHarry now This is for Scripting
+"""
+
+
+print("Subscribe CodeWithHarry now", end=",")
+print("This is for Scripting")
+"""
+output
+Subscribe CodeWithHarry now,This is for Scripting
+"""
+
+print("Subscribe CodeWithHarry now", end=" Joker ")
+print("This is for Scripting")
+"""
+output
+Subscribe CodeWithHarry now Joker This is for Scripting
+"""
+
+print("Subscribe CodeWithHarry now","This is for Scripting")
+"""
+output
+Subscribe CodeWithHarry now This is for Scripting
+"""
+
+# Escape character - escape sequence in python3
+
+print("C:\\Sanjay")
+"""
+output
+C:\Sanjay
+"""
+
+print("C:\'narry")
+print("Harry is \n good boy \t1")
+
+"""
+output
+Harry is
+ good boy
+"""
+
