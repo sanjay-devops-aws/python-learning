9cec40994e505dfa96e5e7744f8c3606c1273ff4
Sanjay <sanjaypn18@gmail.com>
2024-03-21 09:52:40 +0530

    added new calculator

---
+++ calculator-solution.py
@@ -0,0 +1,36 @@
+# a = 50
+# b = 3
+
+# print("The value of", a, "+", 3, "is: ", a+b)
+# print("The value of", a, "-", 3, "is: ", a-b)
+# print("The value of", a, "*", 3, "is: ", a*b)
+# print("The value of", a, "/", 3, "is: ", a/b)
+
+# Output
+# The value of 50 + 3 is:  53
+# The value of 50 - 3 is:  47
+# The value of 50 * 3 is:  150
+# The value of 50 / 3 is:  16.666666666666668
+
+number1 = int(input("Enter the value of no. = "))
+number2 = int(input("Enter the value of Second no. = "))
+
+add = number1 + number2
+print("The sum of ", number1, " and ", number2, "is", add)
+mul = number1 * number2
+print("The multiplication of ", number1, " and ", number2, "is", mul)
+sub = number1 - number2
+print("The subtraction of ", number1, " and ", number2, "is", sub)
+div = number1 / number2
+print("The divison of ", number1, " and ", number2, "is", div)
+flr = number1 // number2
+print("The floor division of ", number1, " and ", number2, "is", flr)
+exp = number1 ** number2
+print("The exponential of ", number1, " and ", number2, "is", exp)
+mud = number1 % number2
+print("The module of", number1, " and ", number2, "is", mud)
+print(":-)")
+
+
+
+
