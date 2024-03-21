fd8e3e51eaaaaa61e55556efe2e4d1d9f1fc1fae
Sanjay <sanjaypn18@gmail.com>
2024-03-21 10:03:55 +0530

    added calculator

--- calculator-solution.py
+++ calculator-solution.py
@@ -1,3 +1,5 @@
+
+
 # a = 50
 # b = 3
 
@@ -32,5 +34,20 @@ print("The module of", number1, " and ", number2, "is", mud)
 print(":-)")
 
 
+# Arithmetic Operators:
+# Addition: 3 + 2
+# Subtraction: 3 -2
+# Multiplication: 3 * 2
+# Division: 3 / 2
+# Floor Division: 3 // 2
+# Exponent: 3 ** 2
+# Modulus: 3 % 2
 
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
 
