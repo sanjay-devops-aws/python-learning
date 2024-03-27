6e063631107f6d8d94da2f2c4f3f0f3969119fd3
Sanjay <sanjaypn18@gmail.com>
2024-03-27 08:44:07 +0530

    update function code

--- function.py
+++ function.py
@@ -92,3 +92,56 @@ def evenOdd(x):
 # Driver code to call the function
 evenOdd(2)
 evenOdd(3)
+
+### Argument pass
+def average(a, b):
+	print("The average is ", (a+b)/2)
+
+average(4, 6)
+
+# output:
+# The average is  5.0
+
+### keyword argument
+def average(a=9, b=1):
+	print("The average is ", (a+b)/2)
+
+average(b=9, a=21)
+
+### Required argument
+def average(a, b, c=1):
+	print("The average is ", (a+ b +c) / 2)
+
+average(5, 6)
+
+### variable lenght argument
+
+def average(*numbers):
+	print(type(numbers))
+	sum =0
+	for i in numbers:
+		sum = sum + i
+	print("Average is:", sum / len(numbers))
+
+average(5, 6, 7, 1)
+
+# output:
+# <class 'tuple'>
+# Average is: 4.75
+
+
+# return statement
+def average(*numbers):
+  print(type(numbers))
+  sum = 0
+  for i in numbers:
+    sum = sum + i
+
+  return sum / len(numbers)
+
+c = average(5, 6, 7, 1)
+print(c)
+
+# output:
+# <class 'tuple'>
+# 4.75
