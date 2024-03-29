213315c1f3fb8b708900a7dcf67f6ac78463a9e8
Sanjay <sanjaypn18@gmail.com>
2024-03-29 09:24:41 +0530

    update import file

--- import-module.py
+++ import-module.py
@@ -1,9 +1,40 @@
-import random
+# import random
 
-courses = ['History', 'Math', 'physics', 'CompSci']
+# courses = ['History', 'Math', 'physics', 'CompSci']
 
-random_course = random.choice(courses)
+# random_course = random.choice(courses)
 
-print(random_course)
+# print(random_course)
 
+###
 
+# import datetime
+# import calendar
+
+# today = datetime.date.today()
+
+# print(today)
+# print(calendar.isleap(2024))
+
+# output:
+# 2024-03-29
+# True
+
+###
+
+import os 
+
+print(os.getcwd()) # current working directory
+
+# output:
+# C:\Users\Sanjay\Documents\python-learning
+
+print(os.__file__)
+ 
+# output:
+# C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.7_3.7.2544.0_x64__qbz5n2kfra8p0\lib\os.py
+
+# import antigravity 
+
+# output:
+# https://xkcd.com/353/
