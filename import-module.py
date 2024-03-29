def9c7d73aa0567eca9a89ab7ea0d03f01443aff
Sanjay <sanjaypn18@gmail.com>
2024-03-29 09:33:42 +0530

    update import file

--- import-module.py
+++ import-module.py
@@ -22,14 +22,14 @@
 
 ###
 
-import os 
+# import os 
 
-print(os.getcwd()) # current working directory
+# print(os.getcwd()) # current working directory
 
-# output:
-# C:\Users\Sanjay\Documents\python-learning
+# # output:
+# # C:\Users\Sanjay\Documents\python-learning
 
-print(os.__file__)
+# print(os.__file__)
  
 # output:
 # C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.7_3.7.2544.0_x64__qbz5n2kfra8p0\lib\os.py
@@ -38,3 +38,11 @@ print(os.__file__)
 
 # output:
 # https://xkcd.com/353/
+
+### this file import module other file 'my_module.py'
+import my_module
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# output:
+# Imported my_module...
