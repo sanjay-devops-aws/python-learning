5756bbdbe5af11551445dd5edebf5fdf15980497
Sanjay <sanjaypn18@gmail.com>
2024-03-31 08:22:03 +0530

    drecursion file added

---
+++ exercise-strftime.py
@@ -0,0 +1,24 @@
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
+
+
+import time
+t = time.strftime('%H:%M:%S') 
+hour = int(time.strftime('%H'))
+hour = int(input("Enter hour: "))
+print(hour)
+
+if(hour>=0 and hour<12):
+  print("Good Morning Sir!")
+elif(hour>=12 and hour<17):
+  print("Good Afternoon Sir!")
+elif(hour>=17 and hour<0):
+  print("Good Night Sir!")
