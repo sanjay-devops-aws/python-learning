520c1e06e26504704f0444fdd79d0bbf421a0c30
Sanjay <sanjaypn18@gmail.com>
2024-04-01 08:58:04 +0530

    pip file added

---
+++ pip_package.py
@@ -0,0 +1,10 @@
+# pip list
+# pip help
+# pip help install
+# pip search Pympler
+# pip list -o
+# pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U  # update all package at time
+# pip list -U setuptool
+# pip freeze
+# pip install -r  test.txt
+# pip install --outdated
