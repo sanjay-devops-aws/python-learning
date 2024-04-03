c3f219da1b9ea2e888b25cf309fa454241346935
Sanjay <sanjaypn18@gmail.com>
2024-04-03 07:59:29 +0530

    sets file added

---
+++ sets.py
@@ -0,0 +1,13 @@
+# Sets are unordered collection of data items
+
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+harry = set()      # empty set mention like
+print(type(harry))
+
+for value in info:
+  print(value)
