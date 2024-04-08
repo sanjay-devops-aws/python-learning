987065c5e3504f220d1a131b58096feebcba5b64
Sanjay <sanjaypn18@gmail.com>
2024-04-08 08:31:24 +0530

    os module file added

---
+++ os-module.py
@@ -0,0 +1,13 @@
+import os
+
+from datetime import datetime
+
+os.chdir('/Users/Sanjay/Documents/python/python-learning')
+
+# mod_time = os.stat('os-demo').st_mtime
+# print(datetime.fromtimestamp(mod_time))
+
+#os.mkdir("os-demofile-create")
+#os.rename('os-demofile-create','os-demo')
+# print(os.listdir())
+
