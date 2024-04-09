fe76dd8fb38596cead55146d560f08e7fae0d94f
Sanjay <sanjaypn18@gmail.com>
2024-04-09 08:03:03 +0530

    import file added

--- import-module.py
+++ import-module.py
@@ -46,3 +46,22 @@ courses = ['History', 'Math', 'Physics', 'CompSci']
 
 # output:
 # Imported my_module...
+
+
+###
+
+# from math import sqrt, pi
+# from math import pi, sqrt as s
+# import math as math_builtin_python
+
+# result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+# print(result)  # Output: 3.0
+
+# from harry import welcome, harry
+# import harry as hr
+# import math
+
+# print(dir(math))
+# print(math.nan, type(math.nan))
+# hr.welcome()
+# print(hr.harry)
